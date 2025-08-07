ddb9df886933c4d82313a6e22aed1fb8d4b0874d
cyan92128505 <cyan92128505@gmail.com>
2025-08-07 17:34:40 +0800

    chore: Add mobile_scanner package and upgrade packages

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,41 +4,44 @@ rm -rfv ./pubspec.lock
 
 # Install all required packages
 fvm flutter pub add \
-  hooks_riverpod \
-  flutter_hooks \
-  riverpod_annotation \
-  freezed_annotation \
-  json_annotation \
-  timezone \
-  uuid \
+  cached_network_image \
+  collection \
+  connectivity_plus \
+  crypto \
   dartz \
+  device_info_plus \
+  equatable \
+  flutter_hooks \
+  flutter_riverpod \
+  flutter_secure_storage \
+  flutter_svg \
+  freezed_annotation \
+  gap \
+  go_router \
+  hooks_riverpod \
   http \
+  injectable \
+  intl \
+  json_annotation \
+  logger \
+  mobile_scanner \
   objectbox \
   objectbox_flutter_libs \
-  shared_preferences \
-  qr_code_scanner \
-  qr_flutter \
-  permission_handler \
-  connectivity_plus \
-  device_info_plus \
   package_info_plus \
   path_provider \
-  crypto \
-  logger \
-  cached_network_image \
-  go_router \
-  flutter_secure_storage \
-  intl \
-  collection \
-  meta \
-  equatable \
-  gap \
-  flutter_screenutil \
-  dev:riverpod_generator \
+  path \
+  permission_handler \
+  qr_flutter \
+  riverpod_annotation \
+  shared_preferences \
+  timezone \
+  uuid \
   dev:build_runner \
   dev:custom_lint \
-  dev:riverpod_lint \
   dev:freezed \
   dev:json_serializable \
+  dev:mockito \
+  dev:mocktail \
   dev:objectbox_generator \
-  dev:mockito
+  dev:riverpod_generator \
+  dev:riverpod_lint \
